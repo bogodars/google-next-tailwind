3489bef0d7ee019a4287f0aec8549d63f7550db8
Bogodar Shmyglyk <bogodar@gmail.com>
2021-04-09 15:17:00 +0300

    footer done

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import Avatar from '../components/Avatar';
 import { ViewGridIcon, MicrophoneIcon } from '@heroicons/react/solid';
 import { SearchIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
+import Footer from '../components/Footer';
+
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center h-screen '>
       <Head>
         <title>Google</title>
         <link rel='icon' href='/favicon.ico' />
@@ -31,24 +33,26 @@ export default function Home() {
       </header>
 
       {/* Body */}
-      <form className='flex flex-col items-center mt-44 flex-grow' action=''>
+      <form
+        className='flex flex-col items-center mt-44 flex-grow w-4/5'
+        action=''>
         <Image
           src='https://s3.amazonaws.com/freebiesupply/large/2x/google-logo-transparent.png'
           height={100}
           width={300}
         />
 
-        <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
-          <SearchIcon className='h-5 mr-3 text-gray-500 ' />
+        <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl  lg:max-w-2xl'>
+          <SearchIcon className='h-5 mr-3 text-gray-500 sm:flex-row sm:space-x-4' />
           <input type='text' className='focus:outline-none flex-grow' />
           <MicrophoneIcon className='h-10 w-10  p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
         </div>
-        <div className='flex'>
+        <div className='flex flex-col w-1/2 space-y-1 justify-center mt-8 sm:flex-row sm:space-x-4 items-center'>
           <button className='btn'>Google Search</button>
           <button className='btn'>I'm Feeling Lucky</button>
         </div>
       </form>
-      {/* Footer */}
+      <Footer />
     </div>
   );
 }
